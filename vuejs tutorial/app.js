2ddb5d507dbcbcb36a5e24454bc607d2d592e0b4
iaihabjb <ihab@ihab-baghdadi.com>
2019-11-15 17:25:55 -0500

    added punch sound with each click and alert at end

--- vuejs tutorial/app.js
+++ vuejs tutorial/app.js
@@ -10,11 +10,19 @@ new Vue({
             this.health -= 10;
             if ( this.health <= 0 ){
                 this.ended = true;
+                window.alert("The game is finished. Start again by closing this alert.")
+                this.ended = true;
             }
         },
         restart: function(){
             this.health = 100;
             this.ended = false;
+        },
+        playSound (sound) {
+          if (sound) {
+            var audio = new Audio(sound);
+            audio.play();
+          }
         }
     },
     computed: {
