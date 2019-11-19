eade637af8537f76fed4440635fefc455c172529
iaihabjb <ihab@ihab-baghdadi.com>
2019-11-19 16:44:28 -0500

    we can style components locally only using scoped

--- vuejs tutorial p2/vuejs-playlist/src/App.vue
+++ vuejs tutorial p2/vuejs-playlist/src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+
 import Ninjas from './Ninjas.vue'
 
 export default {
@@ -20,5 +21,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1{
+  color: purple
+}
 </style>
--- vuejs tutorial p2/vuejs-playlist/src/Ninjas.vue
+++ vuejs tutorial p2/vuejs-playlist/src/Ninjas.vue
@@ -1,7 +1,12 @@
 <template>
+  <div> // used only one root - obligatory
+  <h1>
+    List of ninjas
+  </h1>
   <ul>
     <li v-for="ninja in ninjas">{{ ninja }}</li>
   </ul>
+  </div>
 </template>
 
 <script>
@@ -14,5 +19,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1{
+  color: green;
+}
 </style>
