a96191a839c8bf94747412318bf8807f43915d27
iaihabjb <ihab@ihab-baghdadi.com>
2020-01-01 16:14:59 -0500

    update

--- vue-testing/test/notification.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import test from 'ava'
-import Notifications from '../src/Notifications'
-
-test('that it renders a notification', t => {
-  t.is(Notification.data().message, 'Hello World')
-})
