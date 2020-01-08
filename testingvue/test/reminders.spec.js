3680e229ae83cbf84cb4eb8dc08a519ceff27622
iaihabjb <ihab@ihab-baghdadi.com>
2020-01-08 11:47:44 -0500

    update

--- testingvue/test/reminders.spec.js
+++ testingvue/test/reminders.spec.js
@@ -3,6 +3,12 @@ import expect from 'expect'
 import Reminders from '../src/components/Reminders.vue'
 
 describe ('Reminders', () => {
+  let wrapper
+  
+  beforeEach(() => {
+    wrapper = mount(Reminders)
+  })
+  
   it ('hides the reminders list if there are none', () => {
     let wrapper = mount(Reminders)
     
@@ -10,8 +16,35 @@ describe ('Reminders', () => {
   })
   
   it ('can add reminders', () => {
-    let newReminder = wrapper.find('.new-reminder')
+    let wrapper = mount(Reminders)
     
-    newReminder.element.value
+    addReminder('Go to the store')
+    
+    expect(remindersList()).toContain('Go to the store')
+    expect(remindersList()).toContain('Finish screencast')
   })
+  
+  it ('can remove any reminder', () => {
+    addReminder('Go to the store')
+    addReminder('Finish screencast')
+    
+    let deleteButton = wrapper.find('ul > li:fist-child .remove')
+    
+    deleteButton.trigger('click')
+    
+    expect(remindersList())not.toContain('Go to the store')
+  })
+  
+  function addReminder(body) {
+      let newReminder = wrapper.find('.new-reminder')
+      
+      newReminder.element.value = body
+      newReminder.trigger('input')
+      
+      wrapper.find('button').trigger('click')
+    }
+    
+    function remindersList() {
+      return wrapper.find('ul').text()
+    }
 })
