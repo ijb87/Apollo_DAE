982c988751b7589300775f2b0917391455128f00
iaihabjb <ihab@ihab-baghdadi.com>
2020-01-15 15:57:16 -0500

    quick project

---
+++ Vue quickstart project/vue2-demo-project/src/GithubOutput/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="margin-top: 50px;">
+    <p v-if="currentUsername == null" style="margin-left: 50px; font-family: 'IBM Plex Serif', serif; font-size: 20px">
+      Enter a username above to see their Github data...
+    </p>
+    <p v-else style="margin-left: 50px; font-family: 'IBM Plex Serif', serif; font-size: 20px;">
+      Below are the results for {{ currentUsername }}.
+      <github-user-data :data="githubData[currentUsername]"></github-user-data>
+    </p>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style scoped src="./style.css"></style>
