0fac82f87cb68d19e331802f26549be82c0ba218
iaihabjb <ihab@ihab-baghdadi.com>
2020-02-27 19:30:32 -0500

    'update'

---
+++ A Brief Intro to Using Vue.js with NativeScript/app/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+    <Page>
+        <ActionBar title="Home" />
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <Image
+                    src="https://play.nativescript.org/dist/assets/img/NativeScript_logo.png" />
+                <Button class="btn btn-primary" text="Button" @tap="onButtonTap" />
+            </StackLayout>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+    export default {
+        methods: {
+            onButtonTap() {
+                console.log("Button was pressed");
+            }
+        },
+
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style scoped>
+    .home-panel {
+        vertical-align: center;
+        font-size: 20;
+        margin: 15;
+    }
+
+    .description-label {
+        margin-bottom: 15;
+    }
+</style>
