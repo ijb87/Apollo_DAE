f7ca205a230873f443831a65dc78a58bf6aff764
iaihabjb <ihab@ihab-baghdadi.com>
2020-04-08 13:45:57 -0400

    update

--- Go Intro/gotut.go
+++ Go Intro/gotut.go
@@ -8,26 +8,29 @@ import (
 )
 
 type Sitemapindex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type News struct {
+  Titles []string `xml:"url>news>title"`
+  Keywords []string `xml:"url>news>Keywords"`
+  Locations [string] `xml:sitemap>loc`
 }
 
 func main() {
+  var s Sitemapindex
+  var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
-	var s Sitemapindex
 	xml.Unmarshal(bytes, &s)
 	//fmt.Println(s.Locations)
 
 	for _, Location := range s.Locations {
-		fmt.Printf("%s\n", Location)
+    resp, _ := http.Get("Location")
+  	bytes, _ := ioutil.ReadAll(resp.Body)
+
+  	xml.Unmarshal(bytes, &n)
 	}
 }
