1236f03ea62db7cd62cd58e84aabfc43405e4d4c
iaihabjb <ihab@ihab-baghdadi.com>
2020-04-08 10:14:09 -0400

    go language types

--- Go Intro/gotut.go
+++ Go Intro/gotut.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-  "fmt" 
-  "math/rand"
+  "fmt"
 )
 
+func add(x, y float32) float32  {
+  return x + y
+}
+
+func multiple(a, b string) (string,string) {
+  return a, b
+}
+
 func main() {
-  fmt.Println("A number from 1-100", rand.Intn(100))
+  var a type = 62
+  var b float64 = float64(a)
+
+  x := a
 }
