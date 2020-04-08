0c83767a27e3d628d118ea98c3c22393a01d0894
iaihabjb <ihab@ihab-baghdadi.com>
2020-04-08 11:15:15 -0400

    methods

--- Go Intro/gotut.go
+++ Go Intro/gotut.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
   gas_pedal uint16
   brake_pedal uint16
@@ -9,8 +12,17 @@ type car struct {
   top_speed_kmh float64
 }
 
-fun main (){
-  a_car := car{gas_pedal: 22341, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
+func (c car) kmh() float64 {
+  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+}
+
+func main (){
+  a_car := car{gas_pedal: 65000, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
 
   fmt.Println(a_car.gas_pedal)
+  fmt.Println(a_car.kmh())
 }
