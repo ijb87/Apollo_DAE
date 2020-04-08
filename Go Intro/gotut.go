76de0eda4b5c92ddda628f967ab31508a697ad2a
iaihabjb <ihab@ihab-baghdadi.com>
2020-04-08 13:20:20 -0400

    looping

--- Go Intro/gotut.go
+++ Go Intro/gotut.go
@@ -1,41 +1,33 @@
 package main
 
-// <sitemapindex>
-//    <sitemap>
-//       <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
-//    </sitemap>
-//    <sitemap>
-//       <loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
-//    </sitemap>
-//    <sitemap>
-//       <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
-//    </sitemap>
-// </sitemapindex>
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-import ("fmt"
-        "net/http"
-        "io/ioutil"
-        "encoding/xml")
-
-type SitemapIndex struct {
-  Locations []Location `xml:"sitemap"`
+type Sitemapindex struct {
+	Locations []Location `xml:"sitemap"`
 }
 
 type Location struct {
-  Loc string `xml:"loc"`
+	Loc string `xml:"loc"`
 }
 
-func (L Location) String() string {
-  return fmt.Sprintf(l.Loc)
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
 }
 
 func main() {
-  resp, _ := http.Get("http://www.washingtonpost.com/news-sitemap-index.xml")
-  bytes, _ := ioutil.ReadAll(resp.Body)
-  resp.Body.Close()
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
 
-  var s SitemapIndex
-  xml.Unmarshal(bytes, &s)
+	var s Sitemapindex
+	xml.Unmarshal(bytes, &s)
+	//fmt.Println(s.Locations)
 
-  fmt.Println(s.Locations)
+	for _, Location := range s.Locations {
+		fmt.Printf("%s\n", Location)
+	}
 }
